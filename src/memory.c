271febd2e304098033f940f2ba0892da120b113e
Aiden Mathews <aiden.mathews2002@gmail.com>
2023-12-12 23:33:12 -0600

    Added classes and properties

--- src/memory.c
+++ src/memory.c
@@ -87,6 +87,11 @@ static void blackenObject(Obj *object)
 
     switch (object->type)
     {
+    case OBJ_CLASS: {
+        ObjClass* klass = (ObjClass*)object;
+        markObject((Obj*)klass->name);
+        break;
+    }
     case OBJ_CLOSURE:
     {
         ObjClosure *closure = (ObjClosure *)object;
@@ -102,6 +107,12 @@ static void blackenObject(Obj *object)
         ObjFunction *function = (ObjFunction *)object;
         markObject((Obj *)function->name);
         markArray(&function->chunk.constants);
+        break;
+    }
+    case OBJ_INSTANCE: {
+        ObjInstance* instance = (ObjInstance*)object;
+        markObject((Obj*)instance->klass);
+        markTable(&instance->fields);
         break;
     }
     case OBJ_UPVALUE:
@@ -121,6 +132,10 @@ static void freeObject(Obj *object)
 
     switch (object->type)
     {
+    case OBJ_CLASS: {
+        FREE(ObjClass, object);
+        break;
+    }
     case OBJ_CLOSURE:
     {
         ObjClosure *closure = (ObjClosure *)object;
@@ -133,6 +148,12 @@ static void freeObject(Obj *object)
         ObjFunction *function = (ObjFunction *)object;
         freeChunk(&function->chunk);
         FREE(ObjFunction, object);
+        break;
+    }
+    case OBJ_INSTANCE: {
+        ObjInstance* instance = (ObjInstance*)object;
+        freeTable(&instance->fields);
+        FREE(ObjInstance, object);
         break;
     }
     case OBJ_NATIVE:
